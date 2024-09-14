2ef07d4ea62109784aa26a67a2b4478c92de1bcd
Mathew Allison <matthew.allison.dev@gmail.com>
2024-09-14 11:34:47 -0400

    Initial commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PurchaseOrderDemo.Controllers
+{
+    [Route("[controller]")]
+    public class OrderController : Controller
+    {
+        [HttpGet("Upload")]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadPurchaseOrder")]
+        public async Task<IActionResult> UploadPurchaseOrder(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return Json(new { success = false, message = "No file selected." });
+            }
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            var filePath = Path.Combine(uploadsFolder, file.FileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            var fileUrl = Url.Content($"~/uploads/{file.FileName}");
+            return Json(new { success = true, url = fileUrl });
+        }
+    }
+}
---
+++ Views/Order/Upload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PurchaseOrderDemo.Views.Order
+{
+    public class UploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
